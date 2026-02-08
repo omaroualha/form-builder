0e9dc03d9254d6d6919576f24872cd6ce18641bb
O.Oualha <oualhamar@gmail.com>
2026-02-08 15:17:12 +0100

    🗃️Add Data relations and policy

--- backend/app/Models/Form.php
+++ backend/app/Models/Form.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Form extends Model
 {
@@ -27,6 +28,11 @@ class Form extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function submissions(): HasMany
+    {
+        return $this->hasMany(Submission::class);
     }
 
     public function isPublished(): bool
--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models\Form;
+use App\Policies\FormPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
     public function boot(): void
     {
-        //
+        Gate::policy(Form::class, FormPolicy::class);
     }
 }
